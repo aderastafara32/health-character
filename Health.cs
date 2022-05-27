248b082f72fb5497b1fda71bf612eaa34b865ae3
aderastafara32 <58169938+aderastafara32@users.noreply.github.com>
2022-05-27 20:39:03 +0700

    health character

---
+++ Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+	[SerializeField]
+	int _maximumHealth = 100;
+
+	int _currentHealth = 0;
+
+	public void Start()
+	{
+		_currentHealth = _maximumHealth;
+
+	}
+
+	
+	public void Damage(int damageValue)
+	{
+		_currentHealth -= damageValue;
+		if (_currentHealth <= 0)
+        {
+			Destroy(gameObject);
+        }
+	
+	}
+}
